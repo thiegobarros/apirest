9031fbf093c7f1ea51f05d4635cbf5a6e53a6ee3
Thiego <thiego@flexpeak.com.br>
2020-09-29 00:26:50 -0400

    Inclusão das rotas de editar e excluir

--- src/main/java/com/agendas/apirest/resources/AgendaResource.java
+++ src/main/java/com/agendas/apirest/resources/AgendaResource.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,16 @@ public class AgendaResource {
 	
 	@PostMapping("/agenda")
 	public Agenda salvaAgenda(@RequestBody Agenda agenda){
+		return agendaRepository.save(agenda);
+	}
+	
+	@DeleteMapping("/agenda")
+	public void deletaAgenda(@RequestBody Agenda agenda){
+		agendaRepository.delete(agenda);
+	}
+	
+	@PutMapping("/agenda")
+	public Agenda atualizaAgenda(@RequestBody Agenda agenda){
 		return agendaRepository.save(agenda);
 	}
 }
